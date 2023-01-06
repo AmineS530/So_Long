6b6dd1ac6b46352c478e0e1c0a73c04463ac7fd6
Amine Sadik <asadik@e1r5p12.1337.ma>
2023-01-06 20:02:14 +0100

    let's call it a day

--- map_rendrer.c
+++ map_rendrer.c
@@ -10,6 +10,7 @@ void	map_renderer(t_mapinfo *rdr)
 	void *ptr = mlx_init();
 	void *win = mlx_new_window(ptr,rdr->res * 69, rdr->line_count * 69, "so_long");
 	void *wall = mlx_xpm_file_to_image(ptr, "./Textures/Texture_1.xpm", &width , &height );
+	void *coin = mlx_xpm_file_to_image(ptr, "./Textures/Treasure.xpm", &width , &height );
 	void *backg = mlx_xpm_file_to_image(ptr, "./Textures/bedrock.xpm", &width , &height );
 	while(rdr->map[y])
 	{
@@ -20,6 +21,8 @@ void	map_renderer(t_mapinfo *rdr)
 				mlx_put_image_to_window(ptr , win , wall , x * 69 , y * 69);
 			if (rdr->map[y][x] == '0')
 				mlx_put_image_to_window(ptr , win , backg , x * 69 , y * 69);
+			if (rdr->map[y][x] == 'C')
+				mlx_put_image_to_window(ptr , win , coin , x * 69 , y * 69);
 			x++;
 		}
 		y++;
@@ -27,18 +30,13 @@ void	map_renderer(t_mapinfo *rdr)
 	mlx_loop(ptr);
 }
 
-int y(t_mapinfo *x)
-{
-	int y = 0;
-	while(x->map[y])
-		y++;
-	return y;
-}
-
 int main(int argc, char *argv[])
 {
 	t_mapinfo rdr;
-
-	rdr = process_map(argv[1]);
-	map_renderer(&rdr);
+	if (argc == 2)
+	{
+		rdr = process_map(argv[1]);
+		map_renderer(&rdr);
+	}
+	return (0);
 }
